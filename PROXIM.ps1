3ce233194323fa4245ba0299e5d43fb134e71d2c
Corentin <34584747+GamaniGo@users.noreply.github.com>
2024-08-31 21:55:02 -0400

    Update PROXIM.ps1

--- PROXIM.ps1
+++ PROXIM.ps1
@@ -1,46 +1,46 @@
 function Find-Module {
     if ($null -eq (Get-Module -ListAvailable | Where-Object { $_.Name -eq "Corsinvest.ProxmoxVE.Api" })) {
-        Write-Host "Installing module"
-        Install-Module -Name Corsinvest.ProxmoxVE.Api -Force -Scope CurrentUser
+        Write-Host "Error: Module not found, please install this PowerShell module Corsinvest.ProxmoxVE.Api" -ForegroundColor Red
+        break
     }
 }
 
 function Get-PVECredential {
     while ($true) {
-        $PVEIPServer = Read-Host "Enter the IP address of your Proxmox server"
+        $PVEIPServer = $(Write-Host "Enter the IP address of your Proxmox server : " -ForegroundColor Yellow -NoNewLine; Read-Host )
 
         if ($PVEIPServer -notmatch '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$') {
-            Write-Host "Invalid IP address. Please enter a valid IP address."
+            Write-Host "Error : Invalid IP address. Please enter a valid IP address." -ForegroundColor Red
         } 
         else {
             break
         }
     }
 
-    $choice = Read-Host "For authentication using an API Token, enter '0'; otherwise, leave it blank"
+    $choice = $(Write-Host "For authentication using an API Token, enter '0'; otherwise, leave it blank : " -ForegroundColor Yellow -NoNewline; Read-Host )
 
     if ($choice -eq '0') {
-        $PVEApiKey = Read-Host "Enter your API Token"
+        $PVEApiKey = $(Write-Host "Enter your API Token : " -ForegroundColor Yellow -NoNewline; Read-Host)
 
         try {
             $PveTicket = Connect-PveCluster -HostsAndPorts $PVEIPServer -ApiToken $PVEApiKey -SkipCertificateCheck -ErrorAction Stop
         } 
         catch 
         {
-            Write-Host "Error: $_"
+            Write-Host "Error: $_" -ForegroundColor Red
             Get-PVECredential
         }
 
         return $PveTicket
     } 
     else {
-        $PVEUserName = Read-Host "Enter your user name"
+        $PVEUserName = $(Write-Host "Enter your user name : " -ForegroundColor Yellow -NoNewline; Read-Host)  
 
         try {
             $PveTicket = Connect-PveCluster -HostsAndPorts $PVEIPServer -Credentials (Get-Credential -UserName $PVEUserName) -SkipCertificateCheck -ErrorAction Stop
         } 
         catch {
-            Write-Host "Error: $_"
+            Write-Host "Error: $_" -ForegroundColor Red
             Get-PVECredential
         }
 
@@ -49,57 +49,67 @@ function Get-PVECredential {
 }
 
 function Show-Menu {
-    
-    Write-Host "Please select an option:"
-    Write-Host "1 - Create LXC"
-    Write-Host "2 - Groups & Users"
-    Write-Host "3 - Exit"
 
-    $choice = Read-Host "Enter your choice"
+    # Display the menu header
+    Write-Host " "
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host "Please select an option:" -ForegroundColor Yellow
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host " "
+
+    # Display the menu options
+    Write-Host "1 - " -ForegroundColor Green
+    Write-Host "2 - Groups & Users" -ForegroundColor Green
+    Write-Host "3 - " -ForegroundColor Green
+    Write-Host "4 - Exit" -ForegroundColor Green
+    Write-Host " "
+
+    # Prompt the user for a choice
+    Write-Host "Enter your choice : " -ForegroundColor Yellow -NoNewline
+    $choice = Read-Host
+
+    # Process the user's choice
     switch ($choice) {
-        1 {New-LXC}
-        2 {Show-GroupsUsersMenu}
-        3 { break }
+        1 { }
+        2 { Show-GroupsUsersMenu }
+        3 {  }
+        4 { break }
         default {
             Write-Host " "
+            Write-Host "Error: Invalid choice. Please try again !!!" -ForegroundColor Red
             Write-Host " "
-            Write-Host "Invalid choice. Please try again."
             Show-Menu
         }
     }
 }
 
-function New-LXC {
-    
-    #List Templates
-    $vztmplContent = ((get-pvenodesStorageContent -Node pve-01 -Storage local).ToData() | Where-Object content -eq "vztmpl")
-
-    Write-Host "Here are the templates currently installed on your Proxmox instance"
-    $counter = 1
-    foreach ($tmpl in $vztmplContent.volid)
-    {
-        Write-Host " $counter -> "($tmpl -split "/")[1]
-        $counter++
-    }
-
-}
-
-
 function Show-GroupsUsersMenu {
-    
-    Write-Host "Please select an option:"
-    Write-Host "1 - Create Groups"
-    Write-Host "2 - List Groups"
-    Write-Host "3 - Create Users"
-    Write-Host "4 - List Users"
-    Write-Host "5 - Exit"
 
-    $choice = Read-Host "Enter your choice"
+
+    # Display the menu header
+    Write-Host " "
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host "Please select an option:" -ForegroundColor Yellow
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host " "
+
+    # Display the menu options
+    Write-Host "1 - Create Groups" -ForegroundColor Green
+    Write-Host "2 - List Groups" -ForegroundColor Green
+    Write-Host "3 - Create Users" -ForegroundColor Green
+    Write-Host "4 - List Users" -ForegroundColor Green
+    Write-Host "5 - Exit" -ForegroundColor Green
+    Write-Host " "
+
+    # Prompt the user for a choice
+    Write-Host "Enter your choice : " -ForegroundColor Yellow -NoNewline
+    $choice = Read-Host
+
     switch ($choice) {
         1 {Show-GroupsMenu} #ok
         2 {Get-Groups} #OK
         3 {}
-        4 {}
+        4 {Get-Users} #OK
         5 { Show-Menu }
         default {
             Write-Host " "
@@ -113,13 +123,25 @@ function Show-GroupsUsersMenu {
 }
 
 function Show-GroupsMenu {
-    
-    Write-Host "Please select an option:"
-    Write-Host "1 - Import and Create Groups by CSV"
-    Write-Host "2 - Create individual Group"
-    Write-Host "3 - Exit"
 
-    $choice = Read-Host "Enter your choice"
+
+    # Display the menu header
+    Write-Host " "
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host "Please select an option:" -ForegroundColor Yellow
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host " "
+
+    # Display the menu options
+    Write-Host "1 - Import and Create Groups by CSV" -ForegroundColor Green
+    Write-Host "2 - Create individual Group" -ForegroundColor Green
+    Write-Host "3 - Exit" -ForegroundColor Green
+    Write-Host " "
+
+    # Prompt the user for a choice
+    Write-Host "Enter your choice : " -ForegroundColor Yellow -NoNewline
+    $choice = Read-Host
+
     switch ($choice) {
         1 {New-GroupCSV} #OK
         2 {New-Group} #OK
@@ -136,8 +158,7 @@ function Show-GroupsMenu {
 }
 
 function Get-FilePath {
-    
-    Write-Host "Please select a CSV file to import and create Groups on your Proxmox Server"
+    Write-Host "Please select a CSV file to import and create Groups on your Proxmox Server" -ForegroundColor Yellow
 
     Add-Type -AssemblyName System.Windows.Forms
 
@@ -146,14 +167,12 @@ function Get-FilePath {
         Filter = 'CSV Files (*.csv)|*.csv'
     }
 
-    
-    if ($FileBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK)
-    {
+    if ($FileBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
         $path = $FileBrowser.FileName
-    }
-    else {
-        Write-Host "Warning : No file select ! Please select a CSV file"
+    } else {
+        Write-Host "Warning: No file selected! Please select a CSV file." -ForegroundColor Magenta
         Get-FilePath
+        return
     }
 
     return $path
@@ -163,79 +182,89 @@ function New-GroupCSV {
     $GroupsSuccess = 0
     $GroupsError = 0
     $GroupsWarning = 0
-    
+
     $path = Get-FilePath
+
+    if ($null -eq $path) {
+        Write-Host "Error: No file selected. Please try again." -ForegroundColor Red
+        New-GroupCSV
+        return
+    }
+
     $CSVFile = Import-Csv -Path $path
 
-    if (($CSVFile.GroupName).count -ge 1)
-    {
-        foreach ($groupName in $CSVFile.GroupName)
-        {
-        
-            if ($groupName -notmatch "[^a-zA-Z0-9-_]")
-            {
-            
-                if ((Get-GroupExists $groupName) -EQ $false)
-                {
+    Write-Host " "
+
+    if (($CSVFile.GroupName).Count -ge 1) {
+        foreach ($groupName in $CSVFile.GroupName) {
+            if ($groupName -notmatch "[^a-zA-Z0-9-_]") {
+                if ((Get-GroupExists $groupName) -EQ $false) {
                     try {
                         $command = New-PveAccessGroups -Groupid $groupName -ErrorAction Stop
 
-                        if ($command.IsSuccessStatusCode -eq $true)
-                        {
-                            Write-Host "Success: The group $groupName has been successfully created"
+                        if ($command.IsSuccessStatusCode -eq $true) {
+                            Write-Host "Success: The group $groupName has been successfully created" -ForegroundColor Green
                             $GroupsSuccess++
-                        }
-                        else {
-                            Write-Host "Error: The group $groupName has not been created -> $($command.ReasonPhrase)"
+                        } else {
+                            Write-Host "Error: The group $groupName has not been created -> $($command.ReasonPhrase)" -ForegroundColor Red
                             $GroupsError++
                         }
-                    }
-                    catch {
-                        Write-Host "Error: The group $groupName has not been created -> $_"
+                    } catch {
+                        Write-Host "Error: The group $groupName has not been created -> $_" -ForegroundColor Red
                         $GroupsError++
                     }
-                }
-                else {
-                    Write-Host "Warning: The group $groupName already exists on your Proxmox server"
+                } else {
+                    Write-Host "Warning: The group $groupName already exists on your Proxmox server" -ForegroundColor Magenta
                     $GroupsWarning++
                 }
-            }
-            else {
-                Write-Host "Error: Your group name $groupName contains special characters"
+            } else {
+                Write-Host "Error: Your group name $groupName contains special characters" -ForegroundColor Red
                 $GroupsError++
             }
         }
 
-        Write-Host "Info: The task of creating groups is completed. During the task, there was -> $GroupsSuccess Success -> $GroupsWarning Warning -> $GroupsError Error"
+        Write-Host " "
+        Write-Host " "
 
-        $choice = Read-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu"
-            
-        if ($choice -eq 1)
-        {
+        # Display the information message
+        Write-Host "Info: The task of creating groups is completed. During the task, there was -> " -ForegroundColor Yellow -NoNewline
+
+        # Display the success message in green
+        Write-Host "$GroupsSuccess Success" -ForegroundColor Green -NoNewline
+
+        # Display the warning message in magenta
+        Write-Host " -> $GroupsWarning Warning" -ForegroundColor Magenta -NoNewline
+
+        # Display the error message in red
+        Write-Host " -> $GroupsError Error" -ForegroundColor Red
+
+        Write-Host " "
+        $choice = $(Write-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu : " -ForegroundColor Yellow -NoNewline; Read-Host)
+
+        if ($choice -eq 1) {
             Clear-Host
             New-GroupCSV
-        }
-        else {
+        } else {
             Show-GroupsMenu
         }
-    }
-    else {
+    } else {
+        Write-Host " "
+        Write-Host "Error: No value was found in the GroupName field" -ForegroundColor Red
+        Write-Host " "
+        $choice = $(Write-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu : " -ForegroundColor Yellow -NoNewline; Read-Host)
 
-        $choice = Read-Host "Error : No value was found in the GroupName field. Type 1 to restart the creation of a group or leave it blank to return to the previous menu"
-            
-        if ($choice -eq 1)
-        {
+        if ($choice -eq 1) {
             Clear-Host
             New-GroupCSV
-        }
-        else {
+        } else {
             Show-GroupsMenu
         }
     }
 }
 
 function New-Group {
-    $groupName = Read-Host "Enter name of the new Group"
+    $groupName = $(Write-Host "Enter name of the new Group : " -ForegroundColor Yellow -NoNewline; Read-Host)
+    Write-Host " "
 
     if ((Get-GroupExists $groupName) -EQ $false)
     {
@@ -246,15 +275,16 @@ function New-Group {
 
                 if ($command.IsSuccessStatusCode -eq $true)
                 {
-                    Write-Host "Succes : The group has been successfully created"
+                    Write-Host "Succes : The group has been successfully created" -ForegroundColor Green
                 }
                 else {
-                    Write-Host "Error : " $command.ReasonPhrase
+                    Write-Host "Error : " $command.ReasonPhrase -ForegroundColor Red
                 }
             }
             catch {
-                Write-Host "Error : $_"
-                $choice = Read-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu"
+                Write-Host "Error : $_" -ForegroundColor Red
+                Write-Host " "
+                $choice = $(Write-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu : " -ForegroundColor Yellow -NoNewline; Read-Host)
             
                 if ($choice -eq 1)
                 {
@@ -265,8 +295,9 @@ function New-Group {
                     Show-GroupsMenu
                 }
             }
-
-            $choice = Read-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu"
+            
+            Write-Host " "
+            $choice = $(Write-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu : " -ForegroundColor Yellow -NoNewline; Read-Host)
             
             if ($choice -eq 1)
             {
@@ -276,8 +307,9 @@ function New-Group {
             
         }
         else {
-            Write-Host "Error : Your group name contains special characters"
-            $choice = Read-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu"
+            Write-Host "Error : Your group name contains special characters" -ForegroundColor Red
+            Write-Host " "
+            $choice = Read-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu : "
             
             if ($choice -eq 1)
             {
@@ -287,7 +319,10 @@ function New-Group {
         }
     }
     else {
-        $choice = Read-Host "Warning : The group already exists on your Proxmox server. Type 1 to restart the creation of a group or leave it blank to return to the previous menu"
+        Write-Host "Warning : The group already exists on your Proxmox server." -ForegroundColor Magenta
+        Write-Host " "
+        $choice = $(Write-Host "Type 1 to restart the creation of a group or leave it blank to return to the previous menu : " -ForegroundColor Yellow -NoNewline; Read-Host)
+
         if ($choice -eq 1)
         {
             Clear-Host
@@ -298,7 +333,6 @@ function New-Group {
     Show-GroupsMenu
     
 }
-
 
 function Get-GroupExists { param ( [string]$groupName )
     $GroupExists = $true
@@ -314,36 +348,62 @@ function Get-GroupExists { param ( [string]$groupName )
 function Get-Groups {
     Clear-Host
     $groups = (Get-PveAccessGroups).ToData()
-    Write-Host "Here are the existing groups on your Proxmox server :"
+    Write-Host "Here are the existing groups on your Proxmox server :" -ForegroundColor Yellow
+    Write-Host "=====================================================" -ForegroundColor Cyan
     $groups.groupid
     
     Show-GroupsUsersMenu
 }
 
-function Get-welcome {
-    Write-Host ""
-    Write-Host " ____  ____   _____  _____ __  __ "
-    Write-Host "|  _ \|  _ \ / _ \ \/ /_ _|  \/  |"
-    Write-Host "| |_) | |_) | | | \  / | || |\/| |"
-    Write-Host "|  __/|  _ <| |_| /  \ | || |  | |"
-    Write-Host "|_|   |_| \_\\___/_/\_\___|_|  |_|"
-    Write-Host ""
-    Write-Host ""
-    Write-Host ""
-    Write-Host "PROXIN - PROXIM  - Proxmox Interface Management"
+function Get-Users {
+    $users = (Get-PveAccessUsers).ToData()
+    Write-Host "Here are the existing users on your Proxmox server :" -ForegroundColor Yellow
+    Write-Host "=====================================================" -ForegroundColor Cyan
+    $users.userid
+    
+    Show-GroupsUsersMenu
+}
 
+function Get-Welcome {
+    Clear-Host
+
+    # Define the welcome page information
+    $programName = "PROXIM"
+    $version = "v1.0"
+    $developer = "Corentin"
+    $module = "Corsinvest.ProxmoxVE.Api"
+
+    # Draw the program name in ASCII
+    $asciiArt = @"
+     ____  ____   _____  _____ __  __
+    |  _ \|  _ \ / _ \ \/ /_ _|  \/  |
+    | |_) | |_) | | | \  / | || |\/| |
+    |  __/|  _ <| |_| /  \ | || |  | |
+    |_|   |_| \_\\___/_/\_\___|_|  |_|
+"@
+
+    # Display the welcome page
+    Write-Host $asciiArt 
+    Write-Host " "
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host "Welcome to $programName $version" -ForegroundColor Yellow
+    Write-Host "Developed by: $developer" -ForegroundColor Yellow
+    Write-Host "Based on the module: $module" -ForegroundColor Yellow
+    Write-Host "=============================================" -ForegroundColor Cyan
+    Write-Host " "
+
+    # Optional: Add a pause to allow the user to read the welcome page
+    #Read-Host "Press Enter to continue..."
 }
 
 if (($PSVersionTable.PSVersion.Major) -ge 6) {
 
-    Get-welcome
-    #Find-Module
-    #$PveTicket = Get-PVECredential
+    Get-welcome #ok
+    Find-Module #ok
+    $PveTicket = Get-PVECredential #ok
 
-    #Show-Menu
+    Show-Menu
 
 } else {
     Write-Host "Please update your version of PowerShell. Minimum version requirement for PowerShell is 6.0. You currently have the version $($PSVersionTable.PSVersion)"
 }
-
-
